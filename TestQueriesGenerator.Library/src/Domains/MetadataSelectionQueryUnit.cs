833a684c3cc9d0f51c6356ab8c66f76a7822dfe6
Denis Rezepov <me@dar920910.ru>
2023-10-02 19:08:54 +0300

    Replacement from XmlAttribute to XmlElement for the MetadataSelectionQueryUnit type.

--- TestQueriesGenerator.Library/src/Domains/MetadataSelectionQueryUnit.cs
+++ TestQueriesGenerator.Library/src/Domains/MetadataSelectionQueryUnit.cs
@@ -82,174 +82,174 @@ public record MetadataSelectionQueryUnit
     /// Gets or sets a value indicating whether of the 'Agency' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.Agency)]
+    [XmlElement(Metadata.Agency)]
     public bool Agency { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'Description' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.Description)]
+    [XmlElement(Metadata.Description)]
     public bool Description { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'Department' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.Department)]
+    [XmlElement(Metadata.Department)]
     public bool Department { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'Title' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.Title)]
+    [XmlElement(Metadata.Title)]
     public bool Title { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'Type' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.Type)]
+    [XmlElement(Metadata.Type)]
     public bool Type { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'UserField1' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.UserField1)]
+    [XmlElement(Metadata.UserField1)]
     public bool UserField1 { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'UserField2' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.UserField2)]
+    [XmlElement(Metadata.UserField2)]
     public bool UserField2 { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'UserField3' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.UserField3)]
+    [XmlElement(Metadata.UserField3)]
     public bool UserField3 { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'UserField4' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.UserField4)]
+    [XmlElement(Metadata.UserField4)]
     public bool UserField4 { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'RecordTime' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.RecordTime)]
+    [XmlElement(Metadata.RecordTime)]
     public bool RecordTime { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'ModifiedTime' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.ModifiedTime)]
+    [XmlElement(Metadata.ModifiedTime)]
     public bool ModifiedTime { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'KillDate' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.KillDate)]
+    [XmlElement(Metadata.KillDate)]
     public bool KillDate { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'SOM' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.SOM)]
+    [XmlElement(Metadata.SOM)]
     public bool SOM { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'DAR' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.DAR)]
+    [XmlElement(Metadata.DAR)]
     public bool DAR { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'GOP' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.GOP)]
+    [XmlElement(Metadata.GOP)]
     public bool GOP { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'FileSize' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.FileSize)]
+    [XmlElement(Metadata.FileSize)]
     public bool FileSize { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'Resolution' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.Resolution)]
+    [XmlElement(Metadata.Resolution)]
     public bool Resolution { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'VideoFormat' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.VideoFormat)]
+    [XmlElement(Metadata.VideoFormat)]
     public bool VideoFormat { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'BitRate' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.BitRate)]
+    [XmlElement(Metadata.BitRate)]
     public bool BitRate { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'Handle' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.Handle)]
+    [XmlElement(Metadata.Handle)]
     public bool Handle { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'Link' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.Link)]
+    [XmlElement(Metadata.Link)]
     public bool Link { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'MachineName' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.MachineName)]
+    [XmlElement(Metadata.MachineName)]
     public bool MachineName { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'UserName' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.UserName)]
+    [XmlElement(Metadata.UserName)]
     public bool UserName { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'AudioBits' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.AudioBits)]
+    [XmlElement(Metadata.AudioBits)]
     public bool AudioBits { get; set; }
 
     /// <summary>
     ///  Gets or sets a value indicating whether the 'AudioChannels' metadata field to be enabled to request.
     /// </summary>
     /// <value>Enabled when is 'true' else disabled.</value>
-    [XmlAttribute(Metadata.AudioChannels)]
+    [XmlElement(Metadata.AudioChannels)]
     public bool AudioChannels { get; set; }
 }
